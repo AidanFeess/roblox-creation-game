22244c5330f8f38661e4312a1a51153acb70b400
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2023-10-02 18:59:54 -0500

    Create TierModifiers Index.lua

---
+++ TierModifiers Index.lua
@@ -0,0 +1,16 @@
+local tiermodifiers = {
+	[1] = {
+		TierMultiplier = 1,
+		Color = Color3.fromRGB(255, 255, 255)
+	},
+	[2] = {
+		TierMultiplier = 2,
+		Color = Color3.fromRGB(0, 0, 127)
+	},
+	[3] = {
+		TierMultiplier = 4,
+		Color = Color3.fromRGB(0, 85, 0)
+	},
+}
+
+return tiermodifiers
